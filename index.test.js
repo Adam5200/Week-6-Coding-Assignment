c2b2adbbbaa6018797b6c194ace38036f478cdc5
Adam Hendrickson <106487414+Adam5200@users.noreply.github.com>
2022-07-09 16:44:24 -0500

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -2,8 +2,6 @@ const expect = chai.expect;
 
 describe("Player Classes", function() {
     it("should both have a hand of 26 cards", function() {
-        let game = new Game()
-        game.newGame()
         expect(game.playerOne.hand.length).to.equal(26)
         expect(game.playerTwo.hand.length).to.equal(26)
     })
@@ -11,8 +9,6 @@ describe("Player Classes", function() {
 
 describe("Deck Class", function() {
     it("should have an array of 52 cards", function() {
-        let game = new Game()
-        game.newGame()
         expect(game.deck.cards.length).to.equal(52)
     })
 })
